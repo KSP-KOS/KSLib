KSP-KOS/KSLib

commit 1509d995bb9879b5ce519b2ab89e5df72d8eb2f8

improve readability
While debugging issue #125 I made some minor changes to improve
readability.

library/lib_num_to_formatted_str.ks
@@ -16,18 +16,18 @@ LOCAL FUNCTION time_converter {
 
   LOCAL returnList IS LIST().
   LOCAL localTime IS timeValue.
-
   LOCAL place IS 1.
+
   FOR modValue IN lib_formatting_lex["timeModList"] {
-    LOCAL returnValue IS MOD(localTime,modValue).
+    LOCAL returnValue IS MOD(localTime, modValue).
     returnList:ADD(returnValue).
 
     SET localTime TO FLOOR(localTime / modValue).
     IF localTime = 0 { BREAK. }
     SET place TO place + 1.
     IF place = places { BREAK. }
-
   }
+
   IF localTime > 0 { returnList:ADD(localTime). }
   RETURN returnList.
 }
@@ -42,9 +42,8 @@ FUNCTION time_string {
 
   LOCAL places IS stringList:LENGTH.
   LOCAL timeList IS time_converter(ABS(timeSec), places).
-  LOCAL returnString IS "".
-
   LOCAL maxLength IS MIN(timeList:LENGTH, places).
+  LOCAL returnString IS "".
 
   IF fixedPlaces > 0 {
     UNTIL timeList:LENGTH >= fixedPlaces {
@@ -55,22 +54,20 @@ FUNCTION time_string {
     SET fixedPlaces TO maxLength.
   }
 
-  FROM {LOCAL i IS maxLength - fixedPlaces.} UNTIL i >= maxLength STEP {SET i TO i + 1.} DO {
-    SET returnString TO padding(timeList[i],lib_formatting_lex["leading0List"][i],roundingList[i],FALSE,1) + stringList[i] + returnString.
+  FROM {LOCAL i IS maxLength - fixedPlaces.}
+  UNTIL i >= maxLength STEP {SET i TO i + 1.} DO {
+    LOCAL leading0List IS lib_formatting_lex["leading0List"][i].
+    LOCAL number IS timeList[i].
+    LOCAL pad_str IS padding(number, leading0List, roundingList[i], FALSE, 1).
+    SET returnString TO pad_str + stringList[i] + returnString.
   }
 
   IF timeSec < 0 {
-    IF tMinus {
-      RETURN returnString:INSERT(0,"T- ").
-    } ELSE {
-      RETURN returnString:INSERT(0,"-").
-    }
+    IF tMinus RETURN returnString:INSERT(1, "T- ").
+    ELSE RETURN returnString:INSERT(0, "-").
   } ELSE {
-    IF tMinus {
-      RETURN returnString:INSERT(0,"T+ ").
-    } ELSE {
-      RETURN returnString:INSERT(0," ").
-    }
+    IF tMinus RETURN returnString:INSERT(0, "T+ ").
+    ELSE RETURN returnString:INSERT(0, " ").
   }
 }
 
@@ -92,7 +89,7 @@ FUNCTION time_formatting {
 
   LOCAL roundingList IS LIST(MIN(rounding,2), 0, 0, 0, 0).
   LOCAL formatData IS lib_formatting_lex["timeFormats"][formatType].
-  RETURN time_string(timeSec,formatData[0],formatData[1],roundingList,tMinus).
+  RETURN time_string(timeSec, formatData[0], formatData[1], roundingList, tMinus).
 }
 
 lib_formatting_lex:ADD("siPrefixList",LIST(" y"," z"," a"," f"," p"," n"," μ"," m","  "," k"," M"," G"," T"," P"," E"," Z"," Y")).

commit d864efda32cece0367369d8bd179aa81683ea0a2

fix issue with negative results
1509d99 introduced a bug where negative results displayed like "0T- 1s".
This fixes that and adds a new optional parameter, showPlus, to allow
users to optionally display "+" on positive formats like "+1s" instead
of " 1s".

library/lib_num_to_formatted_str.ks
@@ -34,11 +34,12 @@ LOCAL FUNCTION time_converter {
 
 lib_formatting_lex:ADD("leading0List",LIST(2,2,2,3,3)).
 FUNCTION time_string {
-  PARAMETER timeSec,  // the time in seconds to format
-  fixedPlaces,        // number of required time places (e.g. HH:MM:SS has 3 fixedPlaces)
-  stringList,         // separators for each time place (must have at least fixedPlaces elements!)
-  roundingList,       // rounding factors for each time place (should match length of stringList)
-  tMinus.             // prepend a T- or T+ to format
+  PARAMETER timeSec,    // the time in seconds to format
+  fixedPlaces,          // number of required time places (e.g. HH:MM:SS has 3 fixedPlaces)
+  stringList,           // separators for each time place (must have at least fixedPlaces elements!)
+  roundingList,         // rounding factors for each time place (should match length of stringList)
+  prependT,             // prepend a T- or T+ to format (T- or T if showPlus is FALSE)
+  showPlus IS prependT. // by default only display "+" when prependT is TRUE
 
   LOCAL places IS stringList:LENGTH.
   LOCAL timeList IS time_converter(ABS(timeSec), places).
@@ -63,12 +64,23 @@ FUNCTION time_string {
   }
 
   IF timeSec < 0 {
-    IF tMinus RETURN returnString:INSERT(1, "T- ").
-    ELSE RETURN returnString:INSERT(0, "-").
+    IF prependT {
+      SET returnString TO returnString:INSERT(0, "T- ").
+    } ELSE {
+      SET returnString TO returnString:INSERT(0, "-").
+    }
   } ELSE {
-    IF tMinus RETURN returnString:INSERT(0, "T+ ").
-    ELSE RETURN returnString:INSERT(0, " ").
+    IF prependT {
+      SET returnString TO returnString:INSERT(0, "T+ ").
+    } ELSE IF showPlus {
+      SET returnString TO returnString:INSERT(0, "+").
+    } ELSE {
+      SET returnString TO returnString:INSERT(0, " ").
+    }
   }
+
+
+  RETURN returnString.
 }
 
 //adding list of format types